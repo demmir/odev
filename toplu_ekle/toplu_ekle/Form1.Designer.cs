d5a5c82e05ab63f47eb8239b2f8cd8b5d490b45e
Muhammed Demir <62444268+demmir@users.noreply.github.com>
2021-05-09 17:06:59 +0300

    Add files via upload

---
+++ toplu_ekle/toplu_ekle/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace toplu_ekle
+{
+    partial class toplu_kekle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(795, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Araştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(416, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toplu_kekle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "toplu_kekle";
+            this.Text = "Toplu Kitap Ekle";
+            this.Load += new System.EventHandler(this.toplu_kekle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ toplu_ekle/toplu_ekle/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace toplu_ekle
+{
+    public partial class toplu_kekle : Form
+
+    {
+        SqlConnection conn = new SqlConnection("Data Source = GEFORCE\\SQLEXPRESS; Initial Catalog = toplu_ekle; Integrated Security = True");
+        public toplu_kekle()
+
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Dosya Seç";
+            dialog.FileName = textBox1.Text;
+            dialog.Filter = "Excel Dosyaları|*.xlsx;*.xlsm;*.xlsb;*.xltx;*.xltm;*.xls;*.xlt;*.xls;*.xml;*.xml;*.xlam;*.xla;*.xlw;*.xlr;";
+            dialog.RestoreDirectory = true;
+            dialog.CheckFileExists = false;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+                      
+            {
+
+                textBox1.Text = dialog.FileName;
+
+                //dosya yolu seçildikten sonrası 
+
+                OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + textBox1.Text + ";Extended Properties=Excel 12.0;");
+                //"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;";
+                //"Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + textBox1.Text + "; Extended Properties = \"Excel 8.0;HDR=Yes;IMEX=1\";"
+                conn.Open();
+                OleDbDataAdapter adapt = new OleDbDataAdapter("Select * from[Tablo1]", conn);
+                DataSet sd = new DataSet();
+                DataTable dt = new DataTable();
+                adapt.Fill(dt);
+                this.dataGridView1.DataSource = dt.DefaultView;
+
+
+                
+
+            }
+
+
+
+        }
+
+        void FillGrid()
+        {
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from kitapEkle order by kayıt_ID", conn);
+            DataTable dtab = new DataTable();
+            da.Fill(dtab);
+            dataGridView1.DataSource = dtab;
+            conn.Close();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toplu_kekle_Load(object sender, EventArgs e)
+        {
+            FillGrid();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                SqlCommand cmd = new SqlCommand("Insert into kitapEkle(kitapno,kitapAdi,kitapYazari,baski,basimyili,basimyeri,kitapSayfaSayisi,kitapYayinevi,stok,toplamOkunma)vaues('"+dataGridView1.Rows[i].Cells[2].Value+"')", conn);
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+            FillGrid();
+
+        }
+    }
+}
